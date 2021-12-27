90d48b0b18fcbb099564e9f64b3e7621a72be401
Shirisha-Vulinjala <shirisha.vulinjala@techefolks.com>
2021-12-27 16:44:34 +0530

    added navbar and cart page

--- addcart.js
+++ addcart.js
@@ -1,37 +1,39 @@
-let cartarray=document.querySelectorAll('.cartButton');
+
+let cartarray=document.getElementsByClassName('cartButton');// or we can use querySelectorAll(."classname")
+
 let products=[
     { 
-        name: 'Samsung Galaxy Tab A7 26.31 cm(10.4 inch)',
+        name: 'Samsung Galaxy Tab A7 26.31 cm(10.4 inch),Slim Metal Body,Quad Speakers with Dolby Atoms,RAM 3 GB,ROM 32 GB  Expandable,Wi-Fi only,Gold',
         tag: 'samsungGtabA7',
         price:17789,
         incart:0                                                                                                
     },
     { 
-        name: '2021 Apple 10.2 inch',
+        name: '2021 Apple 10.2-inch (25.91 cm) iPad with A13 Bionic chip (Wi-Fi, 64GB) - Space Grey (9th Generation)',
         tag: 'appleipad',
         price:30900,
         incart:0                                                                                                
     },
     { 
-        name: 'Lenovo Tab M10 HD 2nd Gen',
+        name: 'Lenovo Tab M10 HD 2nd Gen (25.65 cm (10.1 inch), 2 GB, 32 GB, Wi-Fi) with Metallic Body and Octa core Processor',
         tag: 'Lenovotab2ndGen',
         price:9999,
         incart:0                                                                                                
     },
     { 
-        name: 'Lenovo Tab M10 FHD plus Tablet',
+        name: 'Lenovo Tab M10 FHD Plus Tablet (26.16 cm (10.3-inch), 4GB, 128GB, Wi-Fi + LTE, Volte Calling), Platinum Grey',
         tag: 'Lenovo-tab-FHD-plus',
         price:18990,
         incart:0                                                                                                
     },
     { 
         name: 'Samsung Galaxy Tab A7 Lite',
-        tag: 'sam-galaxy-A7-lite',
+        tag: 'Samsung Galaxy Tab A7 Lite 22.05 cm (8.7 inch), Slim Metal Body, Dolby Atmos Sound, RAM 3 GB, ROM 32 GB Expandable, Wi-Fi-only Tablet, Gray',
         price:11883,
         incart:0                                                                                                
     },
     { 
-        name: 'Lenovo Yoga Smart Tablet',
+        name: 'Lenovo Yoga Smart Tablet with The Google Assistant 25.65 cm (10.1 inch, 4GB, 64GB, WiFi + 4G LTE), Iron Grey',
         tag: 'lenovo-yoga-smart',
         price:25999,
         incart:0                                                                                                
@@ -40,13 +42,14 @@ let products=[
 ];
 for(let i=0;i<cartarray.length;i++)
 {
+    
     cartarray[i].addEventListener('click',()=> {
         cartNumbers(products[i]);
         totalCost(products[i]);
 
     })
 }
-function onLoadCartNumbers(){
+function onLoadCartNumbers(){   // independent of event i.e; gets executed when the page loads
     let productNumbers=localStorage.getItem('cartNumbers');
     if(productNumbers){
         document.querySelector('.cart span').textContent=productNumbers;
@@ -72,7 +75,7 @@ setItems(product);
 }
 
 function setItems(product){
-    let cartItems=localStorage.getItem('productsInCart');
+    let cartItems=localStorage.getItem('productsInCart');  // cartitems returns array
     cartItems=JSON.parse(cartItems);
     if(cartItems !=null){
         if(cartItems[product.tag]==undefined){
@@ -81,11 +84,12 @@ function setItems(product){
                 [product.tag]:product
             }
         }
-        cartItems[product.tag].incart+=1;
+        cartItems[product.tag]['incart']+=1;// equivalent to cartItems[product.tag]['incart']=cartItems[product.tag]['incart']+1;
+                                             // we can use .incart or ['incart']
     }
     else{
        
-     product.incart=1;
+        product.incart=1;
      cartItems={
         [product.tag]:product
     }
@@ -113,41 +117,56 @@ function totalCost(product){
 // function for displaying local storage data into cart page
 function displayCart(){
     let cartItems=localStorage.getItem("productsInCart");
+    let cartCost=localStorage.getItem('totalCost');
     cartItems=JSON.parse(cartItems);
     let productContainer=document.querySelector(".productimages");
     if(cartItems&&productContainer){
         productContainer.innerHTML=" ";
         Object.values(cartItems).map(item =>{
             productContainer.innerHTML +=`
-            <div class="product">
-            <ion-icon name="close-circle-outline"></ion-icon>
-            <img src="./tabimages/${item.tag}.jpg">
-            <span>${item.name}</span>
+        <div class="display">
+            <div class="product-title"><span>
+              <ion-icon name="close-circle-outline"></ion-icon></span>
+              <img src="./tabimages/${item.tag}.jpg">
+              <p >
+              ${item.name}
+              </p>
             </div>
+            
             <div class="price">
             ${item.price}
 
-            <div>
-            <div class="quantity">
-            <ion-icon name="remove" class="decrease"></ion-icon>
-            <span>${item.incart}</span>
-            <ion-icon name="add" class="increase"></ion-icon>
             </div>
+                  
+                       
+            <div class="quantity">
+              <button class="btn minus-btn disabled" type="button">-</button>
+              <span>${item.incart}</span>
+              <button class="btn plus-btn" type="button">+</button>
+              
+            </div>
+            
             <div class="total">
-            ${item.incart*item.price}
-            </div>      
+            
+              ${item.incart*item.price}
+            </div>  
+         </div>
+            `;
 
 
-            `
-
-
-        })
+        });
+        productContainer.innerHTML+=`
+        <div class="overAllCostContainer">
+        <h5 class="overAllCostTitle"> OverallPrice</h5>
+        <h5 class="overAllCost">
+        ${cartCost}
+        </h5>`;
 
 
     }
 
-
 }
+
 //whenever we load the page the following function gets executed
 onLoadCartNumbers();
 displayCart();
